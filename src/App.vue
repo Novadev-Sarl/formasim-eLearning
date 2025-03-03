3483b2d89f347a79146b7f47b027573d53e710ee
Dråfølin <derg@drafolin.ch>
2025-03-03 14:18:11 +0100

    :art: feat: Add header and footer components to App layout - Created components - Static import in app root element

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+
+import TheHeader from '@/components/TheHeader.vue'
+import TheFooter from '@/components/TheFooter.vue'
 </script>
 
 <template>
   <header>
+    <TheHeader />
   </header>
 
   <RouterView />
 
   <footer>
+    <TheFooter />
   </footer>
 </template>
