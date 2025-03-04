41368f3d634f7863f35dcc4fd8438fd6ad115e33
Dråfølin <derg@drafolin.ch>
2025-03-04 13:29:28 +0100

    :sparkles: feat: Conditionally render header and footer based on route
    - Added route-based conditional rendering for header and footer
    - Hide header and footer on login page using v-if directive
    - Imported useRoute from vue-router to access current route

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 
 import TheHeader from '@/components/TheHeader.vue'
 import TheFooter from '@/components/TheFooter.vue'
+
+const route = useRoute()
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 flex flex-col w-full bg-neutral-100">
+  <header class="fixed top-0 left-0 flex flex-col w-full bg-neutral-100" v-if="route.path !== '/login'">
     <TheHeader />
   </header>
 
   <RouterView />
 
-  <footer>
+  <footer v-if="route.path !== '/login'">
     <TheFooter />
   </footer>
 </template>
