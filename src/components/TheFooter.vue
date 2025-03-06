b99d840427678855895bdc9afc59fdc3f4f309cc
Dråfølin <derg@drafolin.ch>
2025-03-06 15:31:28 +0100

    :art: feat: Add dynamic copyright year to footer
    - Implemented current year calculation in footer
    - Added copyright text with dynamic year range
    - Included company name and developer attribution
    - Styled footer with responsive layout

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,5 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const year = new Date().getFullYear()
+</script>
 
 <template>
-  <div></div>
+  <div class="flex flex-col w-screen">
+    <!-- Links-->
+    <div></div>
+
+    <!-- Copyright -->
+    <div class="flex flex-col items-center w-full bg-neutral-100">
+      <div class="flex flex-row items-center justify-between w-full py-3 text-sm max-w-7xl">
+        <span>
+          &copy; 2025 {{ year > 2025 ? `- ${year}` : '' }} Formasim. Tous droits réservés.
+        </span>
+
+        <span>Conçu et développé par <a href="https://novadev.ch" class="text-primary">Novadev Sàrl</a></span>
+      </div>
+    </div>
+  </div>
 </template>
