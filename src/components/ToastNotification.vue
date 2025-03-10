cdd958dc8a5a097a95ff890b4fe2dc4f13b7becd
Dråfølin <derg@drafolin.ch>
2025-03-10 17:32:13 +0100

    :lipstick: refactor: Ran prettier

--- src/components/ToastNotification.vue
+++ src/components/ToastNotification.vue
@@ -16,17 +16,24 @@ const emit = defineEmits<{
 </script>
 
 <template>
-  <div class="bg-white rounded-2xl ring-1 ring-neutral-100 p-6 flex flex-row shadow-md items-center gap-4">
+  <div
+    class="bg-white rounded-2xl ring-1 ring-neutral-100 px-6 py-4 flex flex-row shadow-md items-center gap-4"
+  >
     <CheckCircleIcon v-if="props.notification.type === 'success'" class="size-6 text-green-500" />
     <XCircleIcon v-else class="size-6 text-red-500" />
 
-    <div class="flex flex-col gap-2">
-      <span class="font-semibold">{{ props.notification.type === 'success' ? 'Succès' : 'Erreur' }}</span>
+    <div class="flex flex-col gap-1">
+      <span class="font-semibold">{{
+        props.notification.type === 'success' ? 'Succès' : 'Erreur'
+      }}</span>
       <p>{{ props.notification.message }}</p>
     </div>
 
-    <button class="ml-auto p-2 rounded-md cursor-pointer hover:bg-neutral-100" @click="emit('close')">
-      <CloseIcon class="size-6 text-neutral-500" />
+    <button
+      class="ml-auto p-2 rounded-md cursor-pointer hover:bg-neutral-100"
+      @click="emit('close')"
+    >
+      <CloseIcon class="size-4 text-neutral-500" />
     </button>
   </div>
 </template>
